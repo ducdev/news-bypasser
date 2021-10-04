de7c845645a75f13b3853f852a2f374ac0a5aa95
Duc Le <leanhducvt@live.com>
2021-10-05 00:15:12 +0700

    save the full content of nytimes article before it's truncated and restore it after that

--- nytimes.js
+++ nytimes.js
@@ -1,11 +1,15 @@
 console.log('start detecting nytimes.com\'s paywall')
 let checks = 0
+// Save the full content before it's truncated
+const contentElement = document.getElementsByClassName('meteredContent')[0]
+const fullContent = contentElement.innerHTML
+
 const interval = setInterval(() => {
   console.log('checking')
   checks += 1
   if (checks == 40) {
     clearInterval(interval)
-    console.log('stopped checking, max 10 attempts')
+    console.log('stopped checking, max 40 attempts')
   }
   let target = document.getElementById('gateway-content')
   if (target) {
@@ -17,6 +21,9 @@ const interval = setInterval(() => {
 
     target = target.lastChild
     target.className = ''
+
+    // Restore the full content of the article
+    contentElement.innerHTML = fullContent
     clearInterval(interval)
   }
 }, 250)
